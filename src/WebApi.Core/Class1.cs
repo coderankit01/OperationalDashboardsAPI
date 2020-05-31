756c0d920d25a414c872b14e8041d9b116a43e9e
K L Ashritha <ashritha.kl@brillio.com>
2020-05-31 14:26:54 +0000

    Initial Commit

---
+++ src/WebApi.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OperationDashboard.API.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/WebApi.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OperationalDashboard.Web.Api.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
