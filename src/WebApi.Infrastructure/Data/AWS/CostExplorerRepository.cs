7f0df933b74053ccbf49dd1ff532571b4c20c822
coderankit01 <57743344+coderankit01@users.noreply.github.com>
2021-03-31 12:27:09 +0530

    Update CostExplorerRepository.cs
    Remove Hard credentials

--- src/WebApi.Infrastructure/Data/AWS/CostExplorerRepository.cs
+++ src/WebApi.Infrastructure/Data/AWS/CostExplorerRepository.cs
@@ -15,8 +15,7 @@ namespace OperationalDashboard.Web.Api.Infrastructure.Data.AWS
     {
         public async Task<GetCostAndUsageResponse> GetCostAndUsage(GetCostAndUsageRequest costUsageRequest)
         {
-            using (var amazonCostExplorerClient = new AmazonCostExplorerClient("AKIA5SR5QSU3R352WQ6J", "yw4CqQmLlB7CdCKz06Cv2LIsWfLe9AQVvxk8HmKL", RegionEndpoint.GetBySystemName("us-east-1")))
-           // using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
+             using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
             {
                 var response = await amazonCostExplorerClient.GetCostAndUsageAsync(costUsageRequest);
                 return response;
@@ -25,8 +24,7 @@ namespace OperationalDashboard.Web.Api.Infrastructure.Data.AWS
         }
         public async Task<GetCostForecastResponse> GetCostForecast(GetCostForecastRequest costForecastRequest)
         {
-            using (var amazonCostExplorerClient = new AmazonCostExplorerClient("AKIA5SR5QSU3R352WQ6J", "yw4CqQmLlB7CdCKz06Cv2LIsWfLe9AQVvxk8HmKL", RegionEndpoint.GetBySystemName("us-east-1")))
-            //using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
+            using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
             {
                 var response = await amazonCostExplorerClient.GetCostForecastAsync(costForecastRequest);
                 return response;
@@ -35,8 +33,7 @@ namespace OperationalDashboard.Web.Api.Infrastructure.Data.AWS
 
         public async Task<GetRightsizingRecommendationResponse> GetRightsizingRecommendation(GetRightsizingRecommendationRequest rightsizingRecommendationRequest)
         {
-            using (var amazonCostExplorerClient = new AmazonCostExplorerClient("AKIA5SR5QSU3R352WQ6J", "yw4CqQmLlB7CdCKz06Cv2LIsWfLe9AQVvxk8HmKL", RegionEndpoint.GetBySystemName("us-east-1")))
-           //  using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
+             using (var amazonCostExplorerClient = new AmazonCostExplorerClient(awsCredentials, RegionEndpoint.GetBySystemName("us-east-1")))
             {
                 var response = await amazonCostExplorerClient.GetRightsizingRecommendationAsync(rightsizingRecommendationRequest);
                 return response;
